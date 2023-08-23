9bd88a9c0e4dbf52894525b0b7574e401e0a2969
Arik Abdullah <arikabdullah@Ariks-Air.gha.chartermi.net>
2023-08-22 22:44:27 -0400

    Finished preliminary layout.

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
 import { useRouter } from "next/navigation";
-import { TbInnerShadowBottomRightFilled } from "react-icons/tb";
 
 interface ListItemProps {
 	image: string;
@@ -21,6 +21,7 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
 
 	return (
 		<button
+			onClick={onClick}
 			className="
   relative
   group
@@ -37,7 +38,25 @@ const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
 				className="relative
     min-h-[64px]
     min-w-[64px]">
-				<Image />
+				<Image className="object-cover" fill src={image} alt="Image" />
+			</div>
+			<p className="font-medium truncate py-5">{name}</p>
+			<div
+				className="
+      absolute 
+      transition 
+      opacity-0 
+      rounded-full 
+      flex 
+      items-center 
+      justify-center
+      bg-green-500
+      p-4
+      drop-shadow-md
+      right-5
+      group-hover:opacity-100
+      hover:scale-110">
+				<FaPlay className="text-black" />
 			</div>
 		</button>
 	);
